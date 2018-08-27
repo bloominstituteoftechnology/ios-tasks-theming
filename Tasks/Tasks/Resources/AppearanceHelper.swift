66afe482d567f49ef5fcd1944c6a689cedb4773f
Jeremy Taylor <jtaylor@jctaylor.org>
2018-08-27 14:44:27 -0500

    Add custom font and begin customizing appearance

---
+++ Tasks/Tasks/Resources/AppearanceHelper.swift
@@ -0,0 +1,23 @@
+//
+//  AppearanceHelper.swift
+//  Tasks
+//
+//  Created by Jeremy Taylor on 8/27/18.
+//  Copyright © 2018 Andrew R Madsen. All rights reserved.
+//
+
+import UIKit
+
+enum Appearance {
+    static var taskGreen = UIColor(red: 92/255.0, green: 190/255.0, blue: 165/255.0, alpha: 1.0)
+    
+    static func setupAppearance() {
+        let textAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().barTintColor = taskGreen
+        UINavigationBar.appearance().titleTextAttributes = textAttributes
+        UINavigationBar.appearance().largeTitleTextAttributes = textAttributes
+        UISegmentedControl.appearance().tintColor = taskGreen
+        UIBarButtonItem.appearance().tintColor = .white
+        //UILabel.appearance().textColor = .white
+    }
+}
