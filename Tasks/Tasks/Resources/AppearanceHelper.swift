b5438a3118f35d0839def526cc5b3e4375947953
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-27 18:45:57 -0400

    Add functionality for user input and insert placeholder fonts

--- Tasks/Tasks/Resources/AppearanceHelper.swift
+++ Tasks/Tasks/Resources/AppearanceHelper.swift
@@ -10,26 +10,41 @@ import UIKit
 
 enum Appearance {
     
-    static var backgroundBlack = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    static var backgroundYellow = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 191.0/255.0, alpha: 1.0)
     static var purple = UIColor(red: 173.0/255.0, green: 66.0/255.0, blue: 235.0/255.0, alpha: 1.0)
     static var lightPurple = UIColor(red: 194.0/255.0, green: 104.0/255.0, blue: 247.0/255.0, alpha: 1.0)
     
     static func setupAppearance() {
-        UINavigationBar.appearance().barTintColor = backgroundBlack
+        UINavigationBar.appearance().barTintColor = backgroundYellow
         
-        let textAttributes = [NSAttributedStringKey.foregroundColor: lightPurple]
+        let textAttributes = [NSAttributedStringKey.foregroundColor: lightPurple, NSAttributedStringKey.font : titleFont(with: .title2, pointSize: 30)]
+        let largeTextAttributes = [NSAttributedStringKey.foregroundColor: lightPurple, NSAttributedStringKey.font : titleFont(with: .title2, pointSize: 50)]
+
         UINavigationBar.appearance().titleTextAttributes = textAttributes
-        UINavigationBar.appearance().largeTitleTextAttributes = textAttributes
+        UINavigationBar.appearance().largeTitleTextAttributes = largeTextAttributes
+        
         
         UIBarButtonItem.appearance().tintColor = purple
         UISegmentedControl.appearance().tintColor = purple
         
-        UITableViewCell.appearance().backgroundColor = backgroundBlack
+        UITableViewCell.appearance().backgroundColor = backgroundYellow
         
-        UITextField.appearance().keyboardAppearance = .dark
+        //UITextField.appearance().keyboardAppearance = .dark
         UITextField.appearance().tintColor = purple
         UITextView.appearance().tintColor = purple
         
+    }
+    
+    static func titleFont(with textStyle: UIFontTextStyle, pointSize: CGFloat) -> UIFont {
+        let result = UIFont(name: "Avenir next", size: pointSize)!
+        
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: result)
+    }
+    
+    static func tableFont(with textStyle: UIFontTextStyle, pointSize: CGFloat) -> UIFont {
+        let result = UIFont(name: "Avenir next", size: pointSize)!
+        
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: result)
     }
     
 }
