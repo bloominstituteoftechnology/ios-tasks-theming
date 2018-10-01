602fd28d99d921d121b7129c27a3ae7d15393e65
Farhan Farooqui <farhan@farooqui.net>
2018-10-01 16:38:07 -0400

    cell and tableView color changed

--- Tasks/Tasks/Resources/Appearence.swift
+++ Tasks/Tasks/Resources/Appearence.swift
@@ -11,7 +11,7 @@ import UIKit
 enum Appearence {
     
     // MARK :- Properties
-    static var darkGreen = UIColor(red: 34.0/255.0, green: 126.0/255.0, blue: 107.0/255.0, alpha: 1.0)
+    static var darkGreen = UIColor(red: 110.0/255.0, green: 200.0/255.0, blue: 90.0/255.0, alpha: 1.0)
     static var lightGreen = UIColor(red: 73.0/255.0, green: 193.0/255.0, blue: 90.0/255.0, alpha: 1.0)
     
     static func titleFont(with textStyle: UIFontTextStyle, pointSize: CGFloat) -> UIFont {
@@ -39,10 +39,18 @@ enum Appearence {
         
         //Segmented View Controller
         UISegmentedControl.appearance().tintColor = darkGreen
+        
+        //tableView
+        UITableView.appearance().backgroundColor = darkGreen
     }
 
     
     static func setCellStyle(for cell: UITableViewCell){
+    
+        cell.backgroundColor = darkGreen
+        
+        cell.textLabel?.textColor = UIColor.white
+        cell.detailTextLabel?.textColor = UIColor.white
         
         cell.textLabel?.font = self.applicationFont(with: .caption1, pointSize: 15)
         cell.detailTextLabel?.font = self.applicationFont(with: .caption2, pointSize: 10)
@@ -50,8 +58,7 @@ enum Appearence {
         cell.textLabel?.adjustsFontForContentSizeCategory = true
         cell.detailTextLabel?.adjustsFontForContentSizeCategory = true
         
-        cell.textLabel?.textColor = lightGreen
-        cell.detailTextLabel?.textColor = lightGreen
+        
         
     }
     
--- Tasks/Tasks/View Controllers/TasksTableViewController.swift
+++ Tasks/Tasks/View Controllers/TasksTableViewController.swift
@@ -34,6 +34,8 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
         
         let task = fetchedResultsController.object(at: indexPath)
         cell.textLabel?.text = task.name
+        cell.detailTextLabel?.text = task.notes
+        
         Appearence.setCellStyle(for: cell)
         
         return cell
