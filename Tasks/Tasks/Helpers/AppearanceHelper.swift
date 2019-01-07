311b542e7f270b39f39118eded29f28dbd90fb1b
Frulwinn <frulwinn@gmail.com>
2019-01-07 14:43:21 -0800

    added funcs for the two fonts to include for scaling

--- Tasks/Tasks/Helpers/AppearanceHelper.swift
+++ Tasks/Tasks/Helpers/AppearanceHelper.swift
@@ -5,4 +5,14 @@ enum AppearanceHelper {
     static var backgroundBlue = UIColor(named: "MidnightBlue")
     static var vibrantGreen = UIColor(named: "VibrantGreen")
     
+    //allows for accomodating large type
+    static func bebasNeueFont(with textStyle: UIFontTextStyle, pointSize: CGFloat) -> UIFont {
+        guard let font = UIFont(name: "Bebas Neue", size: pointSize) else { fatalError("Font is nil. Check the name of the font.") }
+        return UIFontMetrics(forTextStyle: .callout).scaledFont(for: font)
+    }
+    
+    static func openSansFont(with textStyle: UIFontTextStyle, pointSize: CGFloat) -> UIFont {
+        guard let font = UIFont(name: "Open Sans", size: pointSize) else { fatalError("Font is nil. Check the name of the font.") }
+        return UIFontMetrics(forTextStyle: .callout).scaledFont(for: font)
+    }
 }
