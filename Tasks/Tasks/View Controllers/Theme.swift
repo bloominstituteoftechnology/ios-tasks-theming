78f7c866e74e37175c99c0688627cc535ac5bc70
Lotanna <sugabelly@gmail.com>
2019-01-07 19:51:49 -0500

    Created Theme File
    With struct for appearance changes

---
+++ Tasks/Tasks/View Controllers/Theme.swift
@@ -0,0 +1,14 @@
+//
+//  Theme.swift
+//  Tasks
+//
+//  Created by Lotanna Igwe-Odunze on 1/7/19.
+//  Copyright © 2019 Andrew R Madsen. All rights reserved.
+//
+
+import Foundation
+
+struct Theme {
+    
+    
+}
