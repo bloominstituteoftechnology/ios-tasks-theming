80c7be813aedd20e2d6a215fdc391a0722f61a03
David Doswell <davidoliverdoswell@icloud.com>
2019-01-14 11:49:28 -0800

    set up appearance

---
+++ Tasks/Tasks/Appearance/Appearance.swift
@@ -0,0 +1,28 @@
+//
+//  Appearance.swift
+//  Tasks
+//
+//  Created by David Doswell on 1/14/19.
+//  Copyright © 2019 Andrew R Madsen. All rights reserved.
+//
+
+import UIKit
+
+struct Appearance {
+    static let customColor = UIColor(red: 210.0/255.0, green: 66.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    
+    static func setUp() {
+        UINavigationBar.appearance().barTintColor = customColor
+        UINavigationBar.appearance().tintColor = .white
+
+        UISegmentedControl.appearance().tintColor = customColor
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        UITextField.appearance().tintColor = customColor
+        UITextField.appearance().textColor = customColor
+        
+        UILabel.appearance().textColor = customColor
+        
+        
+    }
+}
