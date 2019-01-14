fca3e90b639f2e54acdf7ffb8d0a41325e9a6ac7
Frulwinn <frulwinn@gmail.com>
2019-01-14 14:04:43 -0800

    added fonts to project and appearance.swift

--- Tasks/Tasks/Appearance.swift
+++ Tasks/Tasks/Appearance.swift
@@ -2,4 +2,15 @@ import UIKit
 
 enum Appearance {
     
+    static func mainFont(pointSize: CGFloat) -> UIFont {
+        let mainTextFont = UIFont(name: "Kwixter", size: pointSize)!
+        return mainTextFont
+    }
+    
+    static func subFont(pointSize: CGFloat) -> UIFont {
+        let subTextFont = UIFont(name: "Barlow Condensed", size: pointSize)!
+        return subTextFont
+    }
+    
+   
 }
