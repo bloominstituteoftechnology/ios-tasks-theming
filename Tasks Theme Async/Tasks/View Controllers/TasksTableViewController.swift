7130ddb8dfa2c6d8561ce32c7a73f327748ff7f9
Sergey Osipyan <osipyansergey@gmail.com>
2019-01-14 14:35:39 -0800

    MVP(Brown)

---
+++ Tasks Theme Async/Tasks/View Controllers/TasksTableViewController.swift
@@ -0,0 +1,166 @@
+//
+//  TasksTableViewController.swift
+//  Tasks
+//
+//  Created by Andrew R Madsen on 8/11/18.
+//  Copyright © 2018 Andrew R Madsen. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TasksTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    @IBAction func refresh(_ sender: Any) {
+        taskController.fetchTasksFromServer { _ in
+            DispatchQueue.main.async {
+                self.refreshControl?.endRefreshing()
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+    super .viewDidLoad()
+    tableView.backgroundColor = AppearanceHelper.backgroundBrown
+    tableView.reloadData()
+        self.navigationController!.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.font: AppearanceHelper.typerighterFont(with: .largeTitle, pointSize: 40)]
+        
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: AppearanceHelper.lambdaTextBrown]
+}
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else { return }
+        header.textLabel?.textColor = AppearanceHelper.lambdaBrown
+       // header.backgroundColor = AppearanceHelper.backgroundBrown
+        header.textLabel?.font = AppearanceHelper.newFont(with: .body, pointSize: 25)
+        header.textLabel?.frame = header.frame
+        header.textLabel?.textAlignment = .center
+    }    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        cell.backgroundColor = AppearanceHelper.lambdaTextBrown
+        let task = fetchedResultsController.object(at: indexPath)
+        cell.textLabel?.text = task.name
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let sectionInfo = fetchedResultsController.sections?[section] else { return nil }
+        return sectionInfo.name.capitalized
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let task = fetchedResultsController.object(at: indexPath)
+            
+            
+            taskController.deleteTaskFromServer(task) { (error) in
+                if let error = error {
+                    NSLog("Error deleting task from server: \(error)")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    let moc = CoreDataStack.shared.mainContext
+                    moc.delete(task)
+                    do {
+                        try moc.save()
+                    } catch {
+                        moc.reset()
+                        NSLog("Error saving managed object context: \(error)")
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: - NSFetchedResultsControllerDelegate
+    
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowTaskDetail" {
+            let detailVC = segue.destination as! TaskDetailViewController
+            if let indexPath = tableView.indexPathForSelectedRow {
+                detailVC.task = fetchedResultsController.object(at: indexPath)
+            }
+            detailVC.taskController = taskController
+        }
+        
+        if segue.identifier == "ShowCreateTask" {
+            let detailVC = segue.destination as! TaskDetailViewController
+            detailVC.taskController = taskController
+        }
+    }
+    
+    // MARK: Properties
+    
+    private let taskController = TaskController()
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Task> = {
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "priority", ascending: true),
+                                        NSSortDescriptor(key: "name", ascending: true)]
+        let moc = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: "priority", cacheName: nil)
+        frc.delegate = self
+        try! frc.performFetch()
+        return frc
+    }()
+}
