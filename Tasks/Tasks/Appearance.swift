85f50afeedb0bcf379fb833111f97f450e349684
Frulwinn <frulwinn@gmail.com>
2019-01-28 12:19:44 -0800

    initial commit

---
+++ Tasks/Tasks/Appearance.swift
@@ -0,0 +1,14 @@
+//
+//  Appearance.swift
+//  Tasks
+//
+//  Created by Lambda_School_Loaner_34 on 1/28/19.
+//  Copyright © 2019 Andrew R Madsen. All rights reserved.
+//
+
+import Foundation
+
+enum Appearance {
+    
+    
+}
