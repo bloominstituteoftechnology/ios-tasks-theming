12a4b5a8985eb2b3aec6dcb533357a8f52abb4b8
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-28 15:16:59 -0500

    Set navigation bar appearance.

--- Tasks/Tasks/Helpers/AppearanceHelper.swift
+++ Tasks/Tasks/Helpers/AppearanceHelper.swift
@@ -6,8 +6,22 @@
 //  Copyright © 2019 Andrew R Madsen. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 enum AppearanceHelper {
     
+    static var lightBlueColor = UIColor(red: 150/255, green: 212/255, blue: 243/255, alpha: 1)
+    
+    static func setupAppearance() {
+        //MARK: - Navigation Bar Appearance
+        UINavigationBar.appearance().barTintColor = lightBlueColor
+        //Set nav bar title color
+        let attributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        UINavigationBar.appearance().titleTextAttributes = attributes
+        UINavigationBar.appearance().largeTitleTextAttributes = attributes
+        
+        //set nav bar button color
+        UIBarButtonItem.appearance().tintColor = .white
+        
+    }
 }
