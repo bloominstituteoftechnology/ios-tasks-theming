92ede51f57116bf8a1ca6d87b10621c1150e902f
Frulwinn <frulwinn@gmail.com>
2019-01-28 12:40:35 -0800

    added font with scaleable ability in appearance and setDarkAppearance in AppDelegate

--- Tasks/Tasks/Appearance.swift
+++ Tasks/Tasks/Appearance.swift
@@ -6,9 +6,21 @@
 //  Copyright © 2019 Andrew R Madsen. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 enum Appearance {
     
+    static func zagFont(with textStyle: UIFontTextStyle, pointSize: CGFloat) -> UIFont {
+        let font = UIFont(name: "Zag Regular", size: pointSize)!
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: font)
+    }
     
+    static func streetFont(with textStyle: UIFontTextStyle, pointSize: CGFloat) -> UIFont {
+        let font = UIFont(name: "Street Cred", size: pointSize)!
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: font)
+    }
+    
+    static func setDarkAppearance() {
+        
+    }
 }
