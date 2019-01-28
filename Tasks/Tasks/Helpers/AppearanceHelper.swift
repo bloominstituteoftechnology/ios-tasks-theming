168efa1b409af0a82aee5095c41b1b512f98332b
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-28 14:59:54 -0500

    Add appearance helper enum

---
+++ Tasks/Tasks/Helpers/AppearanceHelper.swift
@@ -0,0 +1,13 @@
+//
+//  AppearanceHelper.swift
+//  Tasks
+//
+//  Created by Nelson Gonzalez on 1/28/19.
+//  Copyright © 2019 Andrew R Madsen. All rights reserved.
+//
+
+import Foundation
+
+enum AppearanceHelper {
+    
+}
