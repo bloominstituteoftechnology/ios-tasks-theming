8652f2dfe249bfd68cb4e1d4529ef03e137516c2
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-28 15:11:34 -0800

    Fix reload bug.

--- Tasks/Tasks/View Controllers/TasksTableViewController.swift
+++ Tasks/Tasks/View Controllers/TasksTableViewController.swift
@@ -14,6 +14,7 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setAppearance()
+        tableView.reloadData()
     }
 
     @IBAction func refresh(_ sender: Any) {
