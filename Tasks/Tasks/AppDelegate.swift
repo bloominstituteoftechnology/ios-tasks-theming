e4334303958c5fb16ce9113e7b5a742c13f29716
Sean Doyle <sdoylelambda@gmail.com>
2019-02-04 15:04:42 -0500

    Initial commit.

--- Tasks/Tasks/AppDelegate.swift
+++ Tasks/Tasks/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Tasks
-//
-//  Created by Andrew R Madsen on 8/11/18.
-//  Copyright © 2018 Andrew R Madsen. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
