dd5ed3dff85f73309453c5ee7773aae553ffaaa5
Vijay Das <vijaytdas@gmail.com>
2019-02-05 08:12:10 -0600

    add setPurpleThemeAppearace method in helper, called it in AppDelegate

--- Tasks/Tasks/View Controllers/AppearanceHelper.swift
+++ Tasks/Tasks/View Controllers/AppearanceHelper.swift
@@ -8,4 +8,17 @@
 
 import UIKit
 
+enum AppearanceHelper {
+    
+    static var purpleSwatch = UIColor(red: 137/255, green: 90/255, blue: 255/255, alpha: 1)
+    static var lightGray = UIColor(red: 45/255, green: 45/255, blue: 45/255, alpha: 1)
+    
+    static func setPurpleThemeAppearance() {
+        
+        UIBarButtonItem.appearance().tintColor = purpleSwatch
+        UISegmentedControl.appearance().tintColor = purpleSwatch
+    }
+    
+}
+
 
