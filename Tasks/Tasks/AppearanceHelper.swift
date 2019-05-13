e30807e90b4a9ee6a28ad843ef64a2d62d373c02
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-13 13:08:08 -0700

    Rudiment font

--- Tasks/Tasks/AppearanceHelper.swift
+++ Tasks/Tasks/AppearanceHelper.swift
@@ -12,6 +12,8 @@ enum AppearencHelper {
 	static var taskGreen = UIColor(red: 1/255, green: 102/255, blue: 51/255, alpha: 1)
 	static var taskbackGroundColor = UIColor(red: 83/255, green: 87/255, blue: 81/255, alpha: 1)
 	
-	
-	
+	static func rudimentFont(with textStyle: UIFont.TextStyle, pointSize: CGFloat) -> UIFont {
+		let font  = UIFont(name: "Rudiment", size: pointSize)!
+		return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: font)
+	}
 }
