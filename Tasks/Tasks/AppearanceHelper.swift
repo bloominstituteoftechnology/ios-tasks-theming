c1c0fe4ad8f231d6bfa836d5572b0ed640e08e5f
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-13 12:23:37 -0700

    init

---
+++ Tasks/Tasks/AppearanceHelper.swift
@@ -0,0 +1,16 @@
+//
+//  AppearanceHelper.swift
+//  Tasks
+//
+//  Created by Hector Steven on 5/13/19.
+//  Copyright © 2019 Andrew R Madsen. All rights reserved.
+//
+
+import Foundation
+
+enum AppearencHelper {
+	
+	
+	
+	
+}
