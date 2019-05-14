2cf6a72af5fd1566080c0f30da6494789ac54ea5
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-14 16:39:11 -0400

    Add UI Improvements

--- Tasks/Tasks/View Controllers/TasksTableViewController.swift
+++ Tasks/Tasks/View Controllers/TasksTableViewController.swift
@@ -11,6 +11,21 @@ import CoreData
 
 class TasksTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = Appearance.backgroundYellow
+        navigationItem.titleView = imageView
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if let headerView = view as? UITableViewHeaderFooterView {
+            headerView.contentView.backgroundColor = .white
+            headerView.textLabel?.textColor = Appearance.lightPurple
+            headerView.textLabel?.font = Appearance.tableFont(with: .title3, pointSize: 20)
+        }
+    }
+    
     @IBAction func refresh(_ sender: Any) {
         taskController.fetchTasksFromServer { _ in
             DispatchQueue.main.async {
@@ -35,7 +50,14 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
         let task = fetchedResultsController.object(at: indexPath)
         cell.textLabel?.text = task.name
         
+        style(cell: cell)
+        
         return cell
+    }
+    
+    private func style(cell: UITableViewCell) {
+        cell.textLabel?.font = Appearance.tableFont(with: .caption1, pointSize: 18)
+        cell.textLabel?.textColor = Appearance.purple
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -137,6 +159,8 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
     // MARK: Properties
     
     private let taskController = TaskController()
+    @IBOutlet var imageView: UIImageView!
+    
     
     lazy var fetchedResultsController: NSFetchedResultsController<Task> = {
         let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
